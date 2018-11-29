ba19dea20bfaae46727b98f7d90b203b7278ead9
yoshimi-shingai <yoshimi.shingai@linecorp.com>
2018-11-30 08:57:11 +0900

    Initial Commit

---
+++ Segregation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Segregation
+//
+//  Created by SHINGAI YOSHIMI on 2018/11/30.
+//  Copyright © 2018 SHINGAI YOSHIMI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
